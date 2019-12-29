19b6558ba9a690245db1f99b407600fd51846143
mahdi <mahdi.hqz@gmail.com>
2019-12-29 17:16:56 +0330

    movr drst shd

--- main1.c
+++ main1.c
@@ -3,25 +3,44 @@
 #include <string.h>
 #include <math.h>
 
+
 struct mo{
     char name[100];
     int time;
     char imdblink[200];
 };
-struct sa{
-
+struct sans {
+    int t;
+    int mov;
 };
+struct sa{
+    int r;
+    int s;
+    struct sans ss[200];
+};
+
+void movr(struct mo x[],int n){
+    FILE *p;
+    p=fopen("film.txt","r");
+    for (int i = 0; i < n; ++i) {
+        fscanf(p,"%s %d %s",&x[i].name,&x[i].time,&x[i].imdblink);
+    }
+    fclose(p);
+}
 /***************
  *
  * Mahdi HGZ
  *
  ***************/
 int main() {
-    int x, y, m, n, q;
-    FILE *film;
-    film=fopen("film.txt","r");
-    fscanf(film,"%d",&x);
+    int x, y, m, n, q,nmov,nsalon;
+    FILE *p;
+    p=fopen("data.txt","r");
+    fscanf(p,"%d\n%d",&nmov,&nsalon);
+    fclose(p);
+    struct mo movie[nmov];
+    movr(movie,nmov);
+    printf("%s",movie[2].name);
 
-    fclose(film);
     return 0;
 }
